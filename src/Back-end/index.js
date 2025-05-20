5d553bea178719ae5ab4427f5454ad4cad5e82bf
Your Name <buikhanhhung245298@gmail.com>
2025-05-20 17:26:59 +0700

    Them student

--- src/Back-end/index.js
+++ src/Back-end/index.js
@@ -8,6 +8,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(errorMiddleware);
+
 
 app.use('/api/auth', authRoute);
 
@@ -16,5 +18,5 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
-app.use(errorMiddleware);
+
 
