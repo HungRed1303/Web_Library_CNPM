920e9f5fd061cf7db1dc64e766bd3e41646001a1
Red <leviethung1792k4@gmail.com>
2025-05-23 16:27:29 +0700

    setting env front-end

--- src/Front-end/app/logout/page.tsx
+++
@@ -1,9 +0,0 @@
-import LogoutMessage from "@/components/logout-message"
-
-export default function Logout() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black to-gray-900 p-4">
-      <LogoutMessage />
-    </main>
-  )
-}
--- src/Front-end/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/login-form"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black to-gray-900 p-4">
-      <LoginForm />
-    </main>
-  )
-}
